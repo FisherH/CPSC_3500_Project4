175d284cf0cd307cb9112f5179e623b00e166aa8
Michelle Simoni <michelle.simoni779@gmail.com>
2019-03-10 21:02:40 -0700

    commented and also changed struct

--- SocketHelper.h
+++ SocketHelper.h
@@ -1,3 +1,8 @@
+//Authors: Jack Arnold, Fisher Harris, Michelle Smoni
+//description: this class is a helper object for reading from the socket
+//this is to ensure the full message is recieved instead of 
+//sending a partial message
+
 #pragma once
 
 #include <string>
@@ -8,20 +13,27 @@
 
 #define CBSIZE 2048
 
-typedef struct cbuf {
-    char buf[CBSIZE];
+struct cbuf {
+    char buf[CBSIZE]; //creating the buffer
     unsigned int rpos, wpos;  // read and write position in the buffer
-} cbuf_t;
+} ;
 
 
 class SocketHelper
 {
 public:
+	//creating the socket helper buffer
     SocketHelper();
+
+    //reading command
     int ReadLine(char *line, size_t maxLength, int bodyLength=-1);
+    
+    //will reset the rpos and wpos to zero
     void Reset();
+
 
     int read_socket;
 private:
-    cbuf_t cbuf;
+	//cbuf object
+    cbuf messageBuff;
 };
