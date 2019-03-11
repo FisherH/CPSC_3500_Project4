bfba34359b3749507ef1338ebf6890d49e05fb14
Michelle Simoni <michelle.simoni779@gmail.com>
2019-03-10 22:53:45 -0700

    commenting

--- SocketHelper.cpp
+++ SocketHelper.cpp
@@ -1,39 +1,36 @@
-// SocketHelper 
-//
-// Reads \n terminated lines from a socket while using an internal buffer
-// to handle the cases where recv does not return all of the data in a line
-// or returns data that straddles a line feed.
+//Authors: Jack Arnold, Michelle simoni, fisher harris
+//will read from the buffer and also insure that if recv doesnt return all the
+//data that it waits to make sure all data has been recieved
 // 
 #include "SocketHelper.h"
 #include <errno.h>
 #include <cstring>
 
+
+//creating the class
 SocketHelper::SocketHelper()
 {
-    memset(&cbuf, 0, sizeof(cbuf));
+    memset(&messageBuff, 0, sizeof(cbuf));
     read_socket = -1;
 }
 
+//reset function to reset the buffer
 void SocketHelper::Reset()
 {
-    cbuf.rpos = cbuf.wpos = 0;
+    messageBuff.rpos = messageBuff.wpos = 0;
 }
 
-// Read a linee of text from the socket that is terminated by line feed.
-// we use an internal buffer since we recv can return any number of bytes
-// less than what we request.
-// When bodyLength is -1 (default) we read until a line feed is found.  
-// However if bodyLength is specified then  
-// we will simply read bodyLength number of bytes from the socket regardless
-// of contents.  This is used to read the body of a server response
+
+//will read the body length of the bites even if the message received
+//by the server is short
 int SocketHelper::ReadLine(char *dst, size_t size, int bodyLength)
 {
     unsigned int i = 0;
     ssize_t n;
     while (i < size) {
         // no more data in buffer, read it from socket
-        if (cbuf.rpos == cbuf.wpos) {
-            size_t wpos = cbuf.wpos % CBSIZE;
+        if (messageBuff.rpos == messageBuff.wpos) {
+            size_t wpos = messageBuff.wpos % CBSIZE;
             size_t bytesToRead = (CBSIZE - wpos);
 
             // if we are reading the body make sure we don't ask for more
@@ -45,17 +42,17 @@ int SocketHelper::ReadLine(char *dst, size_t size, int bodyLength)
                     bytesToRead = bytesLeft;
             }
 
-            //printf("recv ask %ld\n", bytesToRead);
-            if((n = recv(read_socket, cbuf.buf + wpos, bytesToRead, 0)) < 0) {
+   
+            if((n = recv(read_socket, messageBuff.buf + wpos, bytesToRead, 0)) < 0) {
                 if (errno == EINTR)
                     continue;
                 return -1;
             } else if (n == 0)
                 return 0;
-            cbuf.wpos += n;
-            //printf("recv returned %ld bytes\n", n);
+            messageBuff.wpos += n;
+            
         }
-        dst[i++] = cbuf.buf[cbuf.rpos++ % CBSIZE];
+        dst[i++] = messageBuff.buf[messageBuff.rpos++ % CBSIZE];
         // bail out when line terminated unless we are reading body
         if (dst[i - 1] == '\n' && bodyLength == -1)
             break;
@@ -70,6 +67,6 @@ int SocketHelper::ReadLine(char *dst, size_t size, int bodyLength)
     }
 
     dst[i] = 0;
-    //printf("Readline returning %d bytes\n", i);
+    
     return i;
 }
